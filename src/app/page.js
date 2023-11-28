427f242ede12be93f2ee93af46ed30c6bb406963
Nauf <130215373+naufudge@users.noreply.github.com>
2023-11-28 14:15:48 +0500

    mapped all racks into the layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,139 +2,78 @@
 
 import Image from "next/image"
 import rack from "../../public/assets/border-rack-light.png"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Popup from "@/components/Popup";
 
 export default function Home() {
   const [down, setDown] = useState(false)
-  const [move, setMove] = useState('')
+  const [rackDetail, setRackDetail] = useState('')
   const [popup, showPopup] = useState(false)
 
+  const rack_list = [
+    ["R-09 A", "No Label"],
+    ["R-10 A", "R-07 B"],
+    ["R-10 B", "R-07 A"],
+    ["R-11 A", "R-06 B"],
+    ["R-11 B", "R-06 A"],
+    ["R-12 A", "R-05 B"],
+    ["R-12 B", "R-05 A"],
+    ["R-13 A", "R-04 B"],
+    ["R-13 B", "R-04 A"],
+    ["R-14 A", "R-03 B"],
+    ["R-14 B", "R-03 A"],
+    ["R-15 A", "R-02 B"],
+    ["R-15 B", "R-02 A"],
+    ["R-16 A", "R-01 A"],
+    ["R-16 B", ""],
+    ["R-17 A", ""],
+    ["R-17 B", ""],
+    ["R-18 A", ""],
+    ["R-18 B", ""],
+    ["R-19 A", ""],
+  ]
+
   const RackParentCss = "relative hover:scale-110 hover:cursor-pointer transition-all duration-300"
-  // const RackCss = "rack static mx-auto my-auto hover:cursor-pointer transition-all duration-300"
   const RackCss = "rack static mx-auto my-auto drop-shadow-lg"
-  const rackMovement = "translate-y-[250px] mb-[250px]"
-  const labelCss = "absolute px-2 lg:px-5 lg:py-2 rounded-full bg-[#D4A056] -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-white text-sm md:text-2xl font-semibold z-20"
+  const labelCss = "absolute px-2 lg:px-5 lg:py-2 rounded-full bg- -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-[#D4A056] text-sm md:text-2xl font-semibold z-20"
 
-  const handleMovement = (e) => {
-    setDown(!down)
-    if (!document.getElementById(e.target.id).className.includes(rackMovement)) {
-      document.getElementById(e.target.id).className = `${RackCss} ${rackMovement}`
-      setMove(rackMovement)
-    } else {
-      document.getElementById(e.target.id).className = RackCss
-      setMove('')
-    }
-  }
 
-  const handlePopup = () => {
+  const handlePopup = (detail) => {
     showPopup(!popup)
+    setRackDetail(detail)
   }
 
   return (
-    <div className="mt-10">
-      <div className="relative grid grid-cols-2 gap-7 justify-center place-items-center mx-auto lg:w-3/5 w-11/12 opacity">
-      <div id="1" className={RackParentCss} onClick={() => handlePopup()}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-1"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        />
-      </div>
+    <div className="my-10">
+      <div className="relative grid grid-cols-1 gap-7 justify-center place-items-center mx-auto lg:w-3/5 w-11/12 opacity">
+      {
+        rack_list.map((item, index) => (
+          <div key={index} className="grid grid-cols-2 gap-7">
+            <div id="1" className={RackParentCss} onClick={() => handlePopup(item[0])}>
+              <div className={labelCss}>{item[0]}</div>
+              <Image
+              id={index}
+              src={rack}
+              alt="rack"
+              className={RackCss}
+              />
+            </div>
 
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-2"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-3"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-4"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-5"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-6"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-7"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-8"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-      
-      <div className={RackParentCss}>
-        <div className={labelCss}>R-1</div>
-        <Image
-        id="rack-9"
-        src={rack}
-        alt="rack"
-        className={RackCss}
-        onClick={() => handlePopup()}
-        />
-      </div>
-
-
-
-      <Popup show={popup} close={showPopup} />
+            {item[1] != "" ? 
+              <div className={RackParentCss} onClick={() => handlePopup(item[1])}>
+                <div className={labelCss}>{item[1]}</div>
+                <Image
+                id={index}
+                src={rack}
+                alt="rack"
+                className={`${RackCss} rotate-180`}
+                />
+              </div>
+            : null }
+          </div>
+        ))
+      }
+      <Popup show={popup} close={showPopup} rackName={rackDetail}  />
       </div>
     </div>
   )
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,6 @@
 
 
-const Popup = ({ show, close }) => {
+const Popup = ({ show, close, rackName }) => {
     if (!show) return null;
 
     const handlePopupClose = (e) => { if (e.target.id === "popup") close(false) }
@@ -10,10 +10,10 @@ const Popup = ({ show, close }) => {
         <div
         id='popup'
         onClick={(e) => handlePopupClose(e)}
-        className='blurry-bg fixed inset-0 bg-gray-200 bg-opacity-30 backdrop-blur-md flex justify-center items-center z-50'>
-            <div className="bg-[#333C4B] px-11 py-6 rounded-lg drop-shadow-md text-center flex flex-col gap-2 pop-up">
+        className='blurry-bg fixed inset-0 bg-black bg-opacity-30 backdrop-blur-md flex justify-center items-center z-50'>
+            <div className="bg-[#333C4B] px-11 py-6 rounded-lg drop-shadow-lg text-center flex flex-col gap-2 pop-up">
                 <div>
-                    <h1 className="font-bold text-xl text-[#D4A056]">Details</h1>
+                    <h1 className="font-bold text-xl text-[#D4A056]">{rackName}</h1>
                     <span className="my-3 mx-4 px-2 py-0 right-0 top-0 translate-x-1 rounded-full bg-[#333C4B] text-white text-lg absolute hover:cursor-pointer hover:bg-[#4A4C5C]"
                     onClick={() => close(false)}
                     >x</span>
